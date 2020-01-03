3cc238a53dfd79f96905db3074b386085c243d38
Tom Pesman <tom@9to5.software>
2020-01-03 14:32:59 +0100

    concurrency to 1

--- gridsome/lib/workers/image-processor.js
+++ gridsome/lib/workers/image-processor.js
@@ -113,6 +113,6 @@ exports.process = async function ({ queue, context, cacheDir, backgroundColor })
       throw new Error(`Failed to process image ${relPath}. ${err.message}`)
     }
   }, {
-    concurrency: 2
+    concurrency: 1
   })
 }
