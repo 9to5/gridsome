50a1d0b31c455b7d38568162419593c82c5a538d
Susan Pesman <susan@9to5.software>
2020-01-08 16:35:11 +0100

    use env variable

--- gridsome/lib/utils/sysinfo.js
+++ gridsome/lib/utils/sysinfo.js
@@ -1,6 +1,10 @@
+const physical = require('physical-cpu-count')
+const os = require('os')
+const cpus = os.cpus()
+
 module.exports = {
   cpus: {
-    logical: 2,
-    physical: 2
+    logical: parseInt(process.env.GRIDSOME_CPU_COUNT || cpus.length, 10),
+    physical: parseInt(process.env.GRIDSOME_CPU_COUNT || physical || 1, 10)
   }
 }
